a8eda1a1cd36d1ab655adba6c4413126405a5ff1
dot-ruth <ruthgetaneh5@gmail.com>
2023-05-26 09:29:14 +0300

    added create and read functionality

--- to-do-list/src/components/Todo.js
+++ to-do-list/src/components/Todo.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todo(){
+
+    const [Items,setItems] = useState(
+        [
+            {
+                todoItem:"",
+                complete:false
+            },
+        ]
+    )
+
+    const [todovalue,settodovalue] = useState()
+
+    const handleSubmit = e =>{
+        e.preventDefault() // avoids the page from refreshing everytimr the onSubmit is fired
+        if(todovalue === ""){
+            return ( 
+                <div>
+                    <h3>Pleace enter a task</h3>
+                </div>
+            )
+        }
+        createTodoitem(todovalue)
+        settodovalue("")
+
+    }
+
+    const createTodoitem = (todoItem) =>{
+       const newTodoitem = [...Items,{todoItem,complete:false}]
+       setItems(newTodoitem);
+    }
+
+    
     
 
 
@@ -12,13 +44,27 @@ export default function Todo(){
         </div>
         <div className="d-flex flex-column m-2 border border-primary p-4">
             {/* to do list items */}
-            <label>
-            <input type="Checkbox" value="Placeholder for to do list items"/>  Item 1
-            </label>
+            
+            {Items.map((item) =>{
+                   return (
+                    <label>
+                    <input type="Checkbox" value="{item.todoitem}"/>  {item.todoItem}
+                    </label>
+                   )
+            })}
+            
         </div>
         <div>
             {/* button */}
-            <input className=" border border-primary rounded-top rounded-right rounded-left rounded-bottom m-2" placeholder="Add a New Task " />
+            <form onSubmit={handleSubmit}>
+            <input className=" border border-primary rounded-top rounded-right rounded-left rounded-bottom m-2" placeholder="Add a New Task " 
+            value={todovalue}
+            onChange={(e) => settodovalue(e.target.value)}
+            />
+            <button className="btn btn-primary" onClick={handleSubmit}>
+                Submit
+            </button>
+            </form>
         </div>
         </div>
         </div>
